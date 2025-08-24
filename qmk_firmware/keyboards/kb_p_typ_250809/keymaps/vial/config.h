8a2c70d39b165addfba2e747cace783a1fb6e9c4
he-n-tai <155740624+he-n-tai@users.noreply.github.com>
2025-08-25 02:03:04 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/kb_p_typ_250809/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+#define VIAL_KEYBOARD_UID {0x71, 0x25, 0x68, 0xce, 0x30, 0x58, 0x5a, 0x54}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
